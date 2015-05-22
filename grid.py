c7b09be8afb8db5c7d0ce9ab4f848c424105485a
Luka Skukan <lskukan1@gmail.com>
2015-05-22 13:59:11 +0200

    Set up mildly successful scores for all tasks.
    OK performance was achieved for pair locating, as well as
    positive/negative prediction and score prediction. The resulting code is
    present in the notebook and the three grid searches - grid.py for the
    first task and the c_/r_grid.py for score positivity prediction/score
    value prediction.

--- grid.py
+++ grid.py
@@ -10,23 +10,23 @@ from sklearn.metrics import classification_report, make_scorer
 #Number of folds
 k = 5
 
-X_train = np.loadtxt('features/train/nonscaled.txt')
+X_train = np.loadtxt('features/train/classification.txt')
 y_train = np.loadtxt('features/train/y.txt')[:,0]
 
-X_test = np.loadtxt('features/test/nonscaled.txt')
+X_test = np.loadtxt('features/test/classification.txt')
 y_test = np.loadtxt('features/test/y.txt')[:,0]
 
 X_train = scale(X_train)
 X_test = scale(X_test)
 
 N = X_train.shape[0]
-#kf = KFold(N, n_folds = k) # k-fold validation subsets
-
 
 # ---------- GRID SETUP ----------- #
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 5e-4, 1e-4],
-                     'C': [50, 100, 500, 1000]},
-                    {'kernel': ['linear'], 'C': [10, 50, 100, 500, 1000]}]
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4],
+                     'C': [5, 10, 25, 50, 72, 100, 250, 500, 750, 1000, 1250, 1500]}]#,
+                    #{'kernel': ['linear'], 'C': [10, 50, 100, 500, 1000]}]
+
+# Ignore linear for now, too slow
 
 scores = [('f1',make_scorer(f1_score))]
 
